e4414cb88d1ddfb5a74f3a49e8548a3f8d419a77
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-17 17:51:05 -0500

    refactor User add eventing model

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-// import { User } from './models/User';
+import { User } from './models/User';
 
+const user = new User({ id: 1 });
+
+// user.set({ name: 'new name', age: 120});
+
+user.set({ name: 'no id user', age: 0});
+user.save();
 // const user = new User({ name: 'myname', age: 100});
 
 // user.on('change', () => {
@@ -15,11 +21,11 @@
 // });
 
 // user.trigger('save');
-import axios from 'axios';
+// import axios from 'axios';
 
-axios.post('http://localhost:3000/users', {
-  name: 'myname',
-  age: 20
-})
+// axios.post('http://localhost:3000/users', {
+//   name: 'myname',
+//   age: 20
+// })
 
-axios.get('http://localhost:3000/users/1')
+// axios.get('http://localhost:3000/users/1')
