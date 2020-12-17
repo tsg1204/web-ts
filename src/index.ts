1f00ac64d8c26078214d5556c6a09c3cb49de96f
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-17 16:48:01 -0500

    add event method to User

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import { User } from './models/User';
 
 const user = new User({ name: 'myname', age: 100});
 
-user.set({ name: 'new name', age: 999})
+user.on('change', () => {
 
-console.log(user.get('name'));
+});
+
+user.on('change', () => {
+
+});
+
+user.on('sgfsg', () => {
+
+});
+
+console.log(user);
--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,12 @@ interface UserProps {
   age?: number;
 }
 
+//type alias
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
+
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
@@ -14,4 +19,11 @@ export class User {
   set(update: UserProps): void {
     Object.assign(this.data, update);
   }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
 }
