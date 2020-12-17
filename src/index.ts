1ecef645ec3e525587ebc70005dcb8b754b728eb
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-17 17:09:48 -0500

    add user to db

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
-import { User } from './models/User';
+// import { User } from './models/User';
 
-const user = new User({ name: 'myname', age: 100});
+// const user = new User({ name: 'myname', age: 100});
 
-user.on('change', () => {
-  console.log('change 1')
-});
+// user.on('change', () => {
+//   console.log('change 1')
+// });
 
-user.on('change', () => {
-  console.log('change 2')
-});
+// user.on('change', () => {
+//   console.log('change 2')
+// });
 
-user.on('save', () => {
-console.log('save')
-});
+// user.on('save', () => {
+// console.log('save')
+// });
 
-user.trigger('save');
+// user.trigger('save');
+import axios from 'axios';
 
+axios.post('http://localhost:3000/users', {
+  name: 'myname',
+  age: 20
+})
+
+axios.get('http://localhost:3000/users/1')
