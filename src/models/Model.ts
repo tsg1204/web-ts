18cf17f91e418d2544075139f0c6c811d3b6a861
Tatiana Grigorieva <tsg1204@gmail.com>
2020-12-18 12:13:05 -0500

    add Model class

---
+++ src/models/Model.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosPromise } from 'axios';
+//type alias
+type Callback = () => void;
+
+interface ModelAttributes<T> {
+  set(value: T): void;
+  getAll(): T;
+  get<K extends keyof T>(key: K): T[K];
+}
+
+interface Sync<T> {
+  fetch(id: number): AxiosPromise;
+  save(data: T): AxiosPromise;
+}
+
+interface Events {
+  on(eventName: string, callback: Callback): void;
+  trigger(eventName: string): void;
+}
+
+export class Model {
+
+}
